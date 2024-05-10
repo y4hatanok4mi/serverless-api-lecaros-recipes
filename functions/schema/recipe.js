4c6bba8d0f3b738468eea1c03179c6081e795ac1
y4hatanok4mi <lecarosjc@gmail.com>
2024-05-10 20:04:40 +0800

    Update recipe.js

--- functions/schema/recipe.js
+++ functions/schema/recipe.js
@@ -3,10 +3,6 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const recipeSchema = new Schema ({
-  id: {
-    type: Number,
-    required: true,
-  },
   name: {
     type: String,
     required: true,
